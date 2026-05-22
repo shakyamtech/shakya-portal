f4bbb66fbac7711dc0fdef37bc709251c55b4285
Your Nameshakyamtech <shakya.mahes@gmail.com>
2026-05-23 01:27:20 +0545

    build: remove wrangler.jsonc and vite-plugin-image-optimizer to resolve Cloudflare Pages 500 error

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteImageOptimizer({
-      exclude: /\.svg$/i,
-      png: { quality: 75 },
-      jpeg: { quality: 75 },
-      jpg: { quality: 75 },
-    }),
-  ],
+  plugins: [react()],
 })
+
